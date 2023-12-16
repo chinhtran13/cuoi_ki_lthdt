71e8725040b02f6402f1df901e620c1700f909a8
Pdauser <116711657+Pdauser@users.noreply.github.com>
2023-12-16 22:35:33 +0700

    ....

--- quan_ly_ban_hang/quan_ly_ban_hang/Form7.cs
+++ quan_ly_ban_hang/quan_ly_ban_hang/Form7.cs
@@ -12,19 +12,73 @@ namespace quan_ly_ban_hang
 {
     public partial class Form7 : Form
     {
+        QLYcuahangEntities db = new QLYcuahangEntities();
+        List<SanPham> dsSanPham = new List<SanPham>();
+        SanPham sanPham = null;
         public Form7()
         {
             InitializeComponent();
         }
-
-        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+        void loadDataSanPham(List<SanPham> a)
+        {
+            dataGridViewDoAn.Columns.Add("MaSP", "Mã sản phẩm");
+            dataGridViewDoAn.Columns.Add("TenSP", "Tên sản phẩm");
+            dataGridViewDoAn.Columns.Add("Gia", "Giá");
+            dataGridViewDoAn.Columns.Add("SoLuong", "Số lượng");
+            foreach (SanPham b in a)
+            {
+                if (b.LoaiSP == "Do An")
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.CreateCells(dataGridViewDoAn);
+                    row.Cells[0].Value = b.MaSP + "";
+                    row.Cells[1].Value = b.TenSP + "";
+                    row.Cells[2].Value = b.Gia;
+                    row.Cells[3].Value = b.SoLuong;
+                    dataGridViewDoAn.Rows.Add(row);
+                }
+                else
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    row.CreateCells(dataGridViewThucUong);
+                    row.Cells[0].Value = b.MaSP + "";
+                    row.Cells[1].Value = b.TenSP + "";
+                    row.Cells[2].Value = b.Gia;
+                    row.Cells[3].Value = b.SoLuong;
+                    dataGridViewThucUong.Rows.Add(row);
+                }
+            }
+        }
+        void loadTTDoAn()
+        {
+            txtMaSP.Text = sanPham.MaSP + "";
+            txtTenSP.Text = sanPham.TenSP + "";
+            txtSoLuong.Text = sanPham.SoLuong + "";
+            txtDonGia.Text = sanPham.Gia + "";
+            comboBoxLoaiSP.SelectedIndex = 0;
+        }
+        void loadTTDoUong()
+        {
+            txtMaSP.Text = sanPham.MaSP + "";
+            txtTenSP.Text = sanPham.TenSP + "";
+            txtSoLuong.Text = sanPham.SoLuong + "";
+            txtDonGia.Text = sanPham.Gia + "";
+            comboBoxLoaiSP.SelectedIndex = 1;
+        }
+
+        private void Form7_Load(object sender, EventArgs e)
+        {
+            //lay danh sach san pham
+            dsSanPham = db.SanPhams.ToList();
+            //hien thi tren datagridview
+            loadDataSanPham(dsSanPham);
+            //hien thong tin len cac text khi click vao
+            loadTTDoAn();
+            loadTTDoUong();
         }
     }
 }
